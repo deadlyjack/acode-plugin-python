cca41f22de99f22ec9e8b45ee842d2d847300bc5
Ajit Kumar <ajitkumar@Ajits-Mac-mini.local>
2024-03-02 20:03:06 +0530

    v1.1.1

--- .vscode/getNet.js
+++ .vscode/getNet.js
@@ -1,8 +1,7 @@
 const { networkInterfaces } = require('os');
 
 module.exports = async (mode = 'dev') => {
-  const { WiFi, Ethernet } = getIp();
-  const [ip] = WiFi || Ethernet;
+  const ip = getIp();
   const port = '5500';
   const src = `https://${ip || '10.0.0'}:${port}`;
   console.log('Server starting at: ', src);
@@ -11,20 +10,16 @@ module.exports = async (mode = 'dev') => {
 
 function getIp() {
   const nets = networkInterfaces();
-  const results = {}; // Or just '{}', an empty object
+  let ip = '';
 
-  Object.keys(nets).forEach((name) => {
-    nets[name].forEach((net) => {
-      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-      const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4;
-      if (net.family === familyV4Value && !net.internal) {
-        if (!results[name]) {
-          results[name] = [];
-        }
-        results[name].push(net.address);
-      }
-    });
-  });
-  return results;
+  Object.keys(nets).some((name) => nets[name].find((net) => {
+    const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4;
+    if (net.family === familyV4Value && !net.internal) {
+      ip = net.address;
+      return ip;
+    }
+    return false;
+  }));
+
+  return ip;
 }
